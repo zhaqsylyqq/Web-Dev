7d71baade4d9b57a9249de6337c2754a9d68adb6
nurs <nursultanzhantuar@gmail.com>
2025-01-28 02:12:01 +0500

    Add: Lab2 - facebook progress; Remove: backedn

--- Lab2/backend/README.md
+++
@@ -1,6 +0,0 @@
-### Install and run
-```
-pip install poetry
-poetry install --no-root
-poetry run python src/manage.py runserver
-```
--- Lab2/backend/src/auth_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from .views import LoginView, logout
-
-urlpatterns = [
-    path('login/', LoginView.as_view(), name="login")
-]
